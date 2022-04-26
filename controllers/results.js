0ed81f8e659b99a3cea0843688e292764fe678e7
tom-gordon13 <tombcgordon@gmail.com>
2022-04-25 21:31:06 -0600

    Re-commit with changes saved

--- controllers/results.js
+++ controllers/results.js
@@ -61,22 +61,3 @@ function deleteResult(req, res) {
         return next(err);
     })
 }
-
-// function deleteReview(req, res, next) {
-//     // Note the cool "dot" syntax to query on the property of a subdoc
-//     Movie.findOne({'reviews._id': req.params.id, 'reviews.user': req.user._id}).then(function(movie) {
-//       // Rogue user!
-//       if (!movie) return res.redirect('/movies');
-//       // Remove the review using the remove method available on Mongoose arrays
-//       movie.reviews.remove(req.params.id);
-//       // Save the updated movie
-//       movie.save().then(function() {
-//         // Redirect back to the movie's show view
-//         res.redirect(`/movies/${movie._id}`);
-//       }).catch(function(err) {
-//         // Let Express display an error
-//         return next(err);
-//         // res.redirect(`/movies/${movie._id}`);
-//       });
-//     });
-//   }
